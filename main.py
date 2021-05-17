f9617d8ed11df3ad3f1b016d6d461f1d04292bb4
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-05-17 17:19:05 +0300

    Clean up main.py

--- main.py
+++ main.py
@@ -2,28 +2,30 @@ import Adafruit_DHT
 from time import sleep
 import requests
 
+URL = "https://api-aura.herokuapp.com/api/set-sensor-value"
+
 DHT_SENSOR = Adafruit_DHT.DHT11
 DHT_PIN = 4
-# URL = "https://api-aura.herokuapp.com/api/set-sensor-value"
 
 while True:
-    # Read sensor data
+    # Read DHT data
     humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
 
-    # Success reading sensor value
+    # If both values are defined
     if humidity is not None and temperature is not None:
         # Format sensor data to JSON
         sensor_data = {
             "temperature": temperature,
             "humidity": humidity
         }
+
         print(sensor_data)
 
         # Send POST request
         # response = requests.post(URL, data=sensor_data)
         # print(response.text)
 
-        # Pause for 1 sec
+        # Pause for 1 second
         sleep(1)
     # Failed to read sensor value, tries again
     else:
